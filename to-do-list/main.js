5f5e6c06415e02a43d1d5aa2df4366703ba3b81a
Aevibunny <137563523+Aevibunny@users.noreply.github.com>
2024-04-26 23:08:42 -0700

    created javascript outline

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -5,3 +5,38 @@ import './style.css'
 // document.querySelector('#app').innerHTML = `
 
 // `
+
+const projectContainer = document.getElementById('project-container');
+
+const projectArr = [];
+const currentProject = {};
+
+const createProject = () => {
+
+};
+
+const renderProjects = () => {
+    projectContainer.innerHTML = '';
+
+    projectArr.forEach((project) => projectContainer.innerHTML += 
+    `<div class="project" id="${project.id}">
+        <h6 class="project-title">${project.title}<button class="edit-button">Edit</button> <button class="add-button">+</button></h6>
+        <ul>
+          <li><input type="radio"> Wash Dishes</li>
+          <li><input type="radio"> Wash Laundry</li>
+        </ul>
+     </div>
+    `)
+};
+
+const editProject = () => {
+    
+};
+
+const createTask = () => {
+
+};
+
+const deleteTask = () => {
+    
+};
